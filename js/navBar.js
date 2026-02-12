f4ac136b47c047b927f07b24aa455d52e7b99193
FeedFoodie <admin@northbladetl.com>
2026-02-12 14:04:59 +0800

    fix hover

--- js/navBar.js
+++ js/navBar.js
@@ -1,8 +1,7 @@
 // When the user clicks on the button, toggle between hiding and showing the dropdown content
-function navbarDropdown() {
-	document.getElementById("novelDropdown").classList.toggle("show");
+function navbarDropdown(id) {
+  document.getElementById(id).classList.toggle("show");
 }
-
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(e) {
 	if (!e.target.matches('.dropbtn')) {
